3982f50908769c510c570f0c3d3fa8d17b65618e
AlaBenKhalifa <101175243+AlaBenKhalifa@users.noreply.github.com>
2025-04-29 00:40:16 +0200

    cc

---
+++ Unihelp-microservices-final-User/user/src/main/java/com/unihelp/user/UserApplication.java
@@ -0,0 +1,15 @@
+package com.unihelp.user;
+
+import jakarta.persistence.Enumerated;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+public class UserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserApplication.class, args);
+	}
+
+}
